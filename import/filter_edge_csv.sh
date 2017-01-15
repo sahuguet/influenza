3e2186e81720db78a84a4dbadb5ae4332df4f7c8
Riley Martinez-Lynch <bx+github@teleological.net>
2017-01-15 08:34:23 -0500

    Sunday morning updates
    * New node, edge types
    * Checking in dump of nodes, edges

--- import/filter_edge_csv.sh
+++ import/filter_edge_csv.sh
@@ -1,44 +1,90 @@
 
+# To list edge types:
+# ruby -r csv -ne'row = CSV.parse($_).first; puts "#{row[0]} -#{row[2]}-> #{row[3]}".inspect' edges.csv | sort -u
+
 echo "Filtering edges"
 
-# company-company edges
-head -1 edges.csv | perl -pe's/ //g' > /tmp/edges_companies.csv
-grep ',Debt,' edges.csv >> /tmp/edges_companies.csv
-grep ',Owns,' edges.csv >> /tmp/edges_companies.csv
+echo "Government Organization -Employs-> Person"
+head -1 edges.csv | perl -pe's/ //g' > /tmp/edges_gov_people.csv
+ruby -r csv -ne'row = CSV.parse($_).first; puts $_ if row[0] == "Government Organization" && row[3] == "Person"' edges.csv >> /tmp/edges_gov_people.csv
 
-# person-person edges
-head -1 edges.csv | perl -pe's/ //g' > /tmp/edges_people.csv
-grep ',Reports To,' edges.csv >> /tmp/edges_people.csv
-grep ',Direct Report,' edges.csv >> /tmp/edges_people.csv
-grep ',Nominated,' edges.csv >> /tmp/edges_people.csv
-grep ',Married,' edges.csv >> /tmp/edges_people.csv
-
-# person-gov org edges
-head -1 edges.csv | perl -pe's/ //g' > /tmp/edges_people_gov.csv
-grep ',Runs,' edges.csv >> /tmp/edges_people_gov.csv
-grep ',Member,' edges.csv >> /tmp/edges_people_gov.csv
-
-# person-company edges
-head -1 edges.csv | perl -pe's/ //g' > /tmp/edges_people_cos.csv
-grep ',Owner,' edges.csv >> /tmp/edges_people_cos.csv
-grep ',Advised,' edges.csv >> /tmp/edges_people_cos.csv
-
-# company-person edges
-head -1 edges.csv | perl -pe's/ //g' > /tmp/edges_cos_people.csv
-grep ',PAC Contribution,' edges.csv >> /tmp/edges_cos_people.csv
-grep ',Employs,' edges.csv >> /tmp/edges_cos_people.csv
-
-# gov org-company edges
+echo "Government Organization -Regulatory Action-> Private Sector Company"
 head -1 edges.csv | perl -pe's/ //g' > /tmp/edges_gov_cos.csv
-grep ',Regulatory Action,' edges.csv >> /tmp/edges_gov_cos.csv
+ruby -r csv -ne'row = CSV.parse($_).first; puts $_ if row[0] == "Government Organization" && row[3] == "Private Sector Company"' edges.csv >> /tmp/edges_gov_cos.csv
 
-# person-cabinet edges
+echo "Nonprofit -Paid-> Person"
+head -1 edges.csv | perl -pe's/ //g' > /tmp/edges_nonprofits_people.csv
+ruby -r csv -ne'row = CSV.parse($_).first; puts $_ if row[0] == "Nonprofit" && row[3] == "Person"' edges.csv >> /tmp/edges_nonprofits_people.csv
+
+echo "Person -Nominee-> Cabinet Position"
 head -1 edges.csv | perl -pe's/ //g' > /tmp/edges_people_cabinet.csv
-grep ',Nominee,' edges.csv >> /tmp/edges_people_cabinet.csv
+ruby -r csv -ne'row = CSV.parse($_).first; puts $_ if row[0] == "Person" && row[3] == "Cabinet Position"' edges.csv >> /tmp/edges_people_cabinet.csv
 
-# committee-cabinet edges
+echo "Person -Advised-> Private Sector Company"
+echo "Person -Owner-> Private Sector Company"
+head -1 edges.csv | perl -pe's/ //g' > /tmp/edges_people_cos.csv
+ruby -r csv -ne'row = CSV.parse($_).first; puts $_ if row[0] == "Person" && row[3] == "Private Sector Company"' edges.csv >> /tmp/edges_people_cos.csv
+
+echo "Person -Campaign Contribution-> Person"
+echo "Person -Married-> Person"
+echo "Person -Nominated-> Person"
+echo "Person -Direct Report-> Person"
+echo "Person -PAC Contribution-> Person"
+echo "Person -Reports To-> Person"
+head -1 edges.csv | perl -pe's/ //g' > /tmp/edges_people.csv
+ruby -r csv -ne'row = CSV.parse($_).first; puts $_ if row[0] == "Person" && row[3] == "Person"' edges.csv >> /tmp/edges_people.csv
+
+echo "Person -Founded-> Nonprofit"
+head -1 edges.csv | perl -pe's/ //g' > /tmp/edges_people_nonprofits.csv
+ruby -r csv -ne'row = CSV.parse($_).first; puts $_ if row[0] == "Person" && row[3] == "Nonprofit"' edges.csv >> /tmp/edges_people_nonprofits.csv
+
+echo "Person -Member-> Government Organization"
+echo "Person -Runs-> Government Organization"
+echo "Person -Employed by-> Government Organization"
+head -1 edges.csv | perl -pe's/ //g' > /tmp/edges_people_gov.csv
+ruby -r csv -ne'row = CSV.parse($_).first; puts $_ if row[0] == "Person" && row[3] == "Government Organization"' edges.csv >> /tmp/edges_people_gov.csv
+
+echo "Person -Member-> House Committee"
+echo "Person -Member-> Joint Committee"
+echo "Person -Member-> Senate Committee"
+head -1 edges.csv | perl -pe's/ //g' > /tmp/edges_people_committees.csv
+ruby -r csv -ne'row = CSV.parse($_).first; puts $_ if row[0] == "Person" && ( row[3] =~ /Committee$/ )' edges.csv >> /tmp/edges_people_committees.csv
+
+echo "Person -Nominee-> Ambassador"
+head -1 edges.csv | perl -pe's/ //g' > /tmp/edges_people_ambassador.csv
+ruby -r csv -ne'row = CSV.parse($_).first; puts $_ if row[0] == "Person" && row[3] == "Ambassador"' edges.csv >> /tmp/edges_people_ambassador.csv
+
+echo "Senate Committee -Confirms Nominee-> Ambassador"
+head -1 edges.csv | perl -pe's/ //g' > /tmp/edges_committee_ambassador.csv
+ruby -r csv -ne'row = CSV.parse($_).first; puts $_ if (row[0] =~ /Committee$/) && row[3] == "Ambassador"' edges.csv >> /tmp/edges_committee_ambassador.csv
+
+echo "Political Organization -Employs-> Person"
+head -1 edges.csv | perl -pe's/ //g' > /tmp/edges_political_people.csv
+ruby -r csv -ne'row = CSV.parse($_).first; puts $_ if row[0] == "Political Organization" && row[3] == "Person"' edges.csv >> /tmp/edges_political_people.csv
+
+echo "Private Sector Company -Campaign Contribution-> Person"
+echo "Private Sector Company -Employs-> Person"
+echo "Private Sector Company -PAC Contribution-> Person"
+echo "Private Sector Company -Owned by-> Person"
+head -1 edges.csv | perl -pe's/ //g' > /tmp/edges_cos_people.csv
+ruby -r csv -ne'row = CSV.parse($_).first; puts $_ if row[0] == "Private Sector Company" && row[3] == "Person"' edges.csv >> /tmp/edges_cos_people.csv
+
+echo "Private Sector Company -Debt-> Private Sector Company"
+echo "Private Sector Company -Owns-> Private Sector Company"
+head -1 edges.csv | perl -pe's/ //g' > /tmp/edges_companies.csv
+ruby -r csv -ne'row = CSV.parse($_).first; puts $_ if row[0] == "Private Sector Company" && row[3] == "Private Sector Company"' edges.csv >> /tmp/edges_companies.csv
+
+echo "Private University -Employs-> Person"
+echo "Public University -Employs-> Person"
+head -1 edges.csv | perl -pe's/ //g' > /tmp/edges_school_people.csv
+ruby -r csv -ne'row = CSV.parse($_).first; puts $_ if (row[0] =~ /University$/) && row[3] == "Person"' edges.csv >> /tmp/edges_school_people.csv
+
+echo "Senate Committee -Confirms Nominee-> Cabinet Position"
 head -1 edges.csv | perl -pe's/ //g' > /tmp/edges_committee_cabinet.csv
-grep ',Confirms Nominee,' edges.csv >> /tmp/edges_committee_cabinet.csv
+ruby -r csv -ne'row = CSV.parse($_).first; puts $_ if (row[0] =~ /Committee$/) && row[3] == "Cabinet Position"' edges.csv >> /tmp/edges_committee_cabinet.csv
 
 echo "Filtered edges"
+for file in /tmp/edges_*.csv; do
+  echo "$file: " `tail -n +2 $file | wc -l`
+done
 
--- import/import_nodes.cypher
+++ import/import_nodes.cypher
@@ -1,51 +1,48 @@
 
 USING PERIODIC COMMIT
-LOAD CSV WITH HEADERS FROM "file:///tmp/nodes_cabinet_positions.csv" AS row
-CREATE (:CabinetPosition {name: row.Name});
+LOAD CSV WITH HEADERS FROM "file:///tmp/nodes_ambassadors.csv" AS row
+CREATE (:Ambassador {name: row.Description, country: row.Name});
+CREATE CONSTRAINT ON (a:Ambassador) ASSERT a.name IS UNIQUE; // constraint creates implicit index
+CREATE INDEX ON :Ambassador(country);
 
 USING PERIODIC COMMIT
-LOAD CSV WITH HEADERS FROM "file:///tmp/nodes_committees.csv" AS row
-CREATE (:Committee {name: row.Name, committeeType: row.Type, description: row.Description});
+LOAD CSV WITH HEADERS FROM "file:///tmp/nodes_cabinet_positions.csv" AS row
+CREATE (:CabinetPosition {name: row.Name});
+CREATE CONSTRAINT ON (c:CabinetPosition) ASSERT c.name IS UNIQUE;
 
 USING PERIODIC COMMIT
 LOAD CSV WITH HEADERS FROM "file:///tmp/nodes_gov_orgs.csv" AS row
 CREATE (:GovernmentOrg {name: row.Name, description: row.Description});
+CREATE CONSTRAINT ON (g:GovernmentOrg) ASSERT g.name IS UNIQUE;
+
+USING PERIODIC COMMIT
+LOAD CSV WITH HEADERS FROM "file:///tmp/nodes_committees.csv" AS row
+CREATE (:Committee {name: row.Name, committeeType: row.Type, description: row.Description});
+CREATE CONSTRAINT ON (n:Nonprofit) ASSERT n.name IS UNIQUE;
+
+USING PERIODIC COMMIT
+LOAD CSV WITH HEADERS FROM "file:///tmp/nodes_nonprofits.csv" AS row
+CREATE (:Nonprofit {name: row.Name});
+CREATE CONSTRAINT ON (n:Nonprofit) ASSERT n.name IS UNIQUE;
 
 USING PERIODIC COMMIT
 LOAD CSV WITH HEADERS FROM "file:///tmp/nodes_people.csv" AS row
 CREATE (:Person {name: row.Name, governmentPosition: row.GovernmentPosition, privatePosition: row.PrivateSectorPosition, state: row.State });
+CREATE CONSTRAINT ON (p:Person) ASSERT p.name IS UNIQUE;
+CREATE INDEX ON :Person(state);
 
 USING PERIODIC COMMIT
 LOAD CSV WITH HEADERS FROM "file:///tmp/nodes_political_orgs.csv" AS row
 CREATE (:PoliticalOrg {name: row.Name});
+CREATE CONSTRAINT ON (p:PoliticalOrg) ASSERT p.name IS UNIQUE;
 
 USING PERIODIC COMMIT
 LOAD CSV WITH HEADERS FROM "file:///tmp/nodes_companies.csv" AS row
 CREATE (:Company {name: row.Name});
+CREATE CONSTRAINT ON (c:Company) ASSERT c.name IS UNIQUE;
 
 USING PERIODIC COMMIT
 LOAD CSV WITH HEADERS FROM "file:///tmp/nodes_universities.csv" AS row
 CREATE (:School {name: row.Name, schoolType: row.Type});
-
-// CREATE INDEX ON :CabinetPosition(name);
-CREATE CONSTRAINT ON (c:CabinetPosition) ASSERT c.name IS UNIQUE;
-
-// CREATE INDEX ON :Committee(name);
-CREATE CONSTRAINT ON (c:Committee) ASSERT c.name IS UNIQUE;
-
-// CREATE INDEX ON :GovernmentOrg(name);
-CREATE CONSTRAINT ON (g:GovernmentOrg) ASSERT g.name IS UNIQUE;
-
-// CREATE INDEX ON :Person(name);
-CREATE INDEX ON :Person(state);
-CREATE CONSTRAINT ON (p:Person) ASSERT p.name IS UNIQUE;
-
-// CREATE INDEX ON :PoliticalOrg(name);
-CREATE CONSTRAINT ON (p:PoliticalOrg) ASSERT p.name IS UNIQUE;
-
-// CREATE INDEX ON :Company(name);
-CREATE CONSTRAINT ON (c:Committee) ASSERT c.name IS UNIQUE;
-
-// CREATE INDEX ON :School(name);
 CREATE CONSTRAINT ON (s:School) ASSERT s.name IS UNIQUE;
 
